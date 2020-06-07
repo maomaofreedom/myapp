0caa49046004c02c4488b2d1c3da9308f946e340
xiaomaomao <876042606@qq.com>
2020-06-07 17:14:29 +0800

    texture

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # myapp
+WebGL Samples
 
 > A Vue.js project
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import WebGL from '@/components/WebGL'
 import WebGLDraw from '@/components/WebGLDraw'
+import Texture from '@/components/Texture'
+
 
 Vue.use(Router)
 
@@ -15,6 +17,10 @@ export default new Router({
       path:'/draw',
       name:'WebGLDraw',
       component: WebGLDraw
+    },{
+      path:'/texture',
+      name:'Texture',
+      component:Texture
     }
   ]
 })
